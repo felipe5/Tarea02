b075ca951bc6e4e6802e78ebca3b9577f21d1198
rosavelasquezrojas <rvelasquez.rojas@gmail.com>
2015-05-21 01:37:38 -0300

    I1- Sin boostrap
    Se elimina la libreria, para optimizar tiempo.

--- application/views/Ejemplo/consulta_compleja.php
+++ application/views/Ejemplo/consulta_compleja.php
@@ -1,21 +1,7 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link href="<?php echo base_url('assets/css/bootstrap/js/bootstrap.js');?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/bootstrap/js/bootstrap.min.js');?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/bootstrap/css/bootstrap.css');?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
-
-
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-  </head>
   <body>
-    <table class="table">
+    <table>
       <tr>
         <th>first_name</th>
         <th>last_name</th>
--- application/views/Ejemplo/inicio.php
+++ application/views/Ejemplo/inicio.php
@@ -1,20 +1,6 @@
 <!DOCTYPE html>
 <html>
-	<head>
-    <link href="<?php echo base_url('assets/css/bootstrap/js/bootstrap.js');?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/bootstrap/js/bootstrap.min.js');?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/bootstrap/css/bootstrap.css');?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
-
-
-
-		<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-	</head>
-<body>
+	<body>
 		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_simple">Consulta Simple</a><br>
 		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_media">Consulta Mediana</a><br>
 		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_compleja">Consulta Compleja</a>
